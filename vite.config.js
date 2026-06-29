9a129d0c0ca87dc6ea235d9b3268c99742aa49d3
Vamsi <vamsiram984@gmail.com>
2026-06-29 14:24:41 +0530

    Create static Memory Vault prototype for showcase

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         services: resolve(__dirname, 'services.html'),
         faqs: resolve(__dirname, 'faqs.html'),
         contact: resolve(__dirname, 'contact.html'),
+        memoryVault: resolve(__dirname, 'memory-vault-demo.html'),
       },
     },
   },
